cfb7fbd36f501bd530c4f9d855c4147f389f2755
favinzons <fernandovinzons@gmail.com>
2021-12-21 21:48:13 +0800

    Updated add and index function of Internet Protocol Controller

--- backend/app/Http/Controllers/InternetProtocolController.php
+++ backend/app/Http/Controllers/InternetProtocolController.php
@@ -9,7 +9,8 @@ class InternetProtocolController extends Controller
 {
 
     public function index(){
-
+        $ip_list = InternetProtocol::all();
+        return response()->json(['ip_list' => $ip_list], 200);
     }
 
     public function fetch($id){
@@ -18,6 +19,23 @@ class InternetProtocolController extends Controller
 
     public function add(Request $request){
 
+        try {
+            // No checker as it was not specified if duplicate ip is needed to be checked
+
+            $request->validate([
+                'name' => 'required|max:13|ip',
+                'description' => 'required|max:255'
+            ]);
+
+            $ip = new InternetProtocol;
+            $ip->name = $request->name;
+            $ip->description = $request->description;
+            $ip->save();
+
+            return response()->json(['message' => 'IP Address Added Successfully'], 200);
+        } catch (\Throwable $th) {
+            throw $th;
+        }
 
     }
 
