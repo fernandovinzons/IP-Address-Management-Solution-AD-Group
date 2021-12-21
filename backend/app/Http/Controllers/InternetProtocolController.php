53e94a4a9365806f16a50fa7124a680aff772183
favinzons <fernandovinzons@gmail.com>
2021-12-21 21:53:41 +0800

    Updated fetch and update function of InternetProtocolController

--- backend/app/Http/Controllers/InternetProtocolController.php
+++ backend/app/Http/Controllers/InternetProtocolController.php
@@ -8,15 +8,25 @@ use Illuminate\Http\Request;
 class InternetProtocolController extends Controller
 {
 
+    // Fetch All IP Address
     public function index(){
         $ip_list = InternetProtocol::all();
         return response()->json(['ip_list' => $ip_list], 200);
     }
 
+    // Fetch Specific IP Address
     public function fetch($id){
+        $ip = InternetProtocol::find($id);
 
+        if($ip){
+            return response()->json(['ip' => $ip], 200);
+        }
+        else{
+            return response()->json(['message' => 'IP Address not found'], 404);
+        }
     }
 
+    // Add IP Address
     public function add(Request $request){
 
         try {
@@ -39,9 +49,24 @@ class InternetProtocolController extends Controller
 
     }
 
+    // Update an existing IP Address
     public function update(Request $request, $id){
 
+        $request->validate([
+            'description' => 'required|max:255'
+        ]);
 
+        $ip = InternetProtocol::find($id); // Check if IP address is already added
+
+        if($ip){
+            $ip->description = $request->description;
+            $ip->update();
+
+            return response()->json(['message' => 'IP Address Label Updated Successfully'], 200);
+        }
+        else{
+            return response()->json(['message' => 'IP Address not found'], 404);
+        }
 
     }
 }
